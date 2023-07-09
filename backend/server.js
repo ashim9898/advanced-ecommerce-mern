ddf10e0e85c4a4873db7a7468f70a77dc0649f90
ashimpoudyal98 <75020083+ashimpoudyal@users.noreply.github.com>
2023-07-09 12:25:50 +0545

    Product CRUD done

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const app = require("./app")
 
 const dotenv = require("dotenv")
-
+const connectDb = require('./config/database')
 //config
 dotenv.config({path:"backend/config/config.env"})
+
+//connecting to database
+connectDb()
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`server is working on http://localhost:${process.env.PORT}`)
